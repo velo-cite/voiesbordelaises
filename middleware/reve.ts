2910b0a85b40146ee759928be1313f0861bf67b4
Paul LOPEZ <p.lopez@alienor.net>
2024-04-22 19:21:31 +0200

    pow pow pow +2 lignes par rapport à lyon

--- middleware/reve.ts
+++ middleware/reve.ts
@@ -1,5 +1,5 @@
 export default defineNuxtRouteMiddleware(to => {
-  const voieLyonnaiseRegex = /reve-(1[0-2]|[1-9])\b/;
+  const voieLyonnaiseRegex = /reve-(1[0-4]|[1-9])\b/;
   const isVoieLyonnaiseValid = voieLyonnaiseRegex.test(to.fullPath);
   if (!isVoieLyonnaiseValid) {
     return navigateTo('/404');
