c4557fb9a7281a610859f32f5013423cfa64e14f
Paul LOPEZ <p.lopez@alienor.net>
2024-04-22 23:23:51 +0200

    récupération des couleurs du ReVE bis

--- composables/useColors.ts
+++ composables/useColors.ts
@@ -1,18 +1,20 @@
 export const useColors = () => {
   function getLineColor(line: number): string {
     const colors = new Map();
-    colors.set(1, '#60A75B');
-    colors.set(2, '#AC4D35');
-    colors.set(3, '#3B7B64');
-    colors.set(4, '#DC8953');
-    colors.set(5, '#AF7392');
-    colors.set(6, '#396083');
-    colors.set(7, '#75BCAE');
-    colors.set(8, '#7E6D98');
-    colors.set(9, '#EAAB50');
-    colors.set(10, '#9A8A4B');
-    colors.set(11, '#4DADC9');
-    colors.set(12, '#DBABB7');
+    colors.set(1, '#2f780b');
+    colors.set(2, '#1ce8de');
+    colors.set(3, '#e3af95');
+    colors.set(4, '#746040');
+    colors.set(5, '#1c77b7');
+    colors.set(6, '#fff6fd');
+    colors.set(7, '#ae03ff');
+    colors.set(8, '#e743b1');
+    colors.set(9, '#fb7d22');
+    colors.set(10, '#dde03a');
+    colors.set(11, '#b8319d');
+    colors.set(12, '#e14351');
+    colors.set(13, '#a0a0a0');
+    colors.set(14, '#40ca46');
     return colors.get(line);
   }
 
